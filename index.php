380230c60f729ac2bf82c7364bb1a5520920ebf0
NEIL GAUTAM <42885532+neilgautam@users.noreply.github.com>
2020-03-21 00:20:25 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,31 +1,54 @@
 <!DOCTYPE html>
-<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<title>ABC Hospital</title>
+	
 </head>
 <body>
 	<header>
 		<div class="upmost">
-		<h2 class = "logo">NEIL GAUTAM SUPERSPECIALITY</h2>
+		<h2 class = "logo">ABC SUPERSPECIALITY</h2>
 		<h3 class  = "logo">HOSPITALS & MEDICAL COLLEGES</h3>
 		<a href="#" class="login">LOGIN</a>
 		</div>
-		<nav>
-			<div>
-				<ul>
-					<li><a href="#">PATIENT CARE</a></li>
-					<li><a href="#">CENTER OF EXCELLENCE</a></li>
-					<li><a href="#">ACADEMICS AND RESEARCH</a></li>
-					<li><a href="#">LOCATIONS</a></li>
-					<li><a href="#">CONTACT US</a></li>
+		<nav class = "head-master">
+			<div class = "head">
+				<ul class = "head-ulist">
+					<li class ="head-list" ><a href="#" class = "head-a">PATIENT CARE</a></li>
+					<li class ="head-list"><a href="#" class = "head-a">CENTER OF EXCELLENCE</a></li>
+					<li class ="head-list"><a href="#" class = "head-a">ACADEMICS AND RESEARCH</a></li>
+					<li class ="head-list"><a href="#" class = "head-a">LOCATIONS</a></li>
+					<li class ="head-list"><a href="#" class = "head-a">CONTACT US</a></li>
 				</ul>
 			</div>
 		</nav>		
 	</header>
-	<main></main>
-	<footer></footer>
+	<main>
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d56013.54697334635!2d77.14118227910154!3d28.66430889999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d028a115fdd77%3A0x8059cd42b6381024!2sApollo%20Hospital!5e0!3m2!1sen!2sin!4v1584716905524!5m2!1sen!2sin" width="600" height="450" frameborder="0" style="border:0; width:100%;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+	</main>
+	<footer class="foot-main">
+		<div class="footer-div">
+			<nav class= "foot-master">
+				<ul class= "foot">
+					<li class = "foot-heading"><a href="#" class="heading" >Education</a></li>
+					<li class = "foot-heading"><a href="" class="points">Introduction</a></li>
+					<li class = "foot-heading"><a href="" class="points">DNB Courses</a></li>
+					<li class = "foot-heading"><a href="" class="points">Medical Courses</a></li>
+					<li class = "foot-heading"><a href="" class="points">Nursing</a></li>
+					<li class = "foot-heading"><a href="" class="points">ANS</a></li>
+				</ul>
+				<ul class= "foot">
+					<li class = "foot-heading"><a href="#" class="heading" >Contact Us</a></li>
+					<li class = "foot-heading"><a href="" class="points">Institue of medical sciences</a></li>
+					<li class = "foot-heading"><a href="" class="points">Fax No: 011-22938484</a></li>
+					<li class = "foot-heading"><a href="" class="points">Landline:011-27363836</a></li>
+					<li class = "foot-heading"><a href="" class="points">Important E-mail Addresses</a></li>
+				</ul>
+			</nav>
+			
+		</div>
+	</footer>
 </body>
 </html>
