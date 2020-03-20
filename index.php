87ff0081d2a375ec72fe1a62bb18f0744950ab42
NEIL GAUTAM <42885532+neilgautam@users.noreply.github.com>
2020-03-20 18:01:24 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>ABC Hospital</title>
+</head>
+<body>
+	<header>
+		<div class="upmost">
+		<h2 class = "logo">NEIL GAUTAM SUPERSPECIALITY</h2>
+		<h3 class  = "logo">HOSPITALS & MEDICAL COLLEGES</h3>
+		<a href="#" class="login">LOGIN</a>
+		</div>
+		<nav>
+			<div>
+				<ul>
+					<li><a href="#">PATIENT CARE</a></li>
+					<li><a href="#">CENTER OF EXCELLENCE</a></li>
+					<li><a href="#">ACADEMICS AND RESEARCH</a></li>
+					<li><a href="#">LOCATIONS</a></li>
+					<li><a href="#">CONTACT US</a></li>
+				</ul>
+			</div>
+		</nav>		
+	</header>
+	<main></main>
+	<footer></footer>
+</body>
+</html>
