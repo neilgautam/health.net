f092e5c9daaaf3f9ec3740ea9370fd3316d4185a
NEIL GAUTAM <42885532+neilgautam@users.noreply.github.com>
2020-04-19 02:17:56 +0530

    Add files via upload

---
+++ demographics.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type = "text/css"  href="style.css">
+	<title>ABC Hospital</title>
+	
+</head>
+<body>
+	<header>
+		<div class="upmost">
+		<h2 class = "logo" >ABC SUPERSPECIALITY <br>HOSPITALS & MEDICAL COLLEGES</h2>
+		</div>
+		<nav class = "head-master">
+				<ul class = "head-ulist">
+					<li class ="head-list"><a href="index.php" class = "head-a">HOME</a></li>
+					<li class ="head-list"><a href="demographics.php" class = "head-a">DEMOGRAPHICS</a></li>
+					<li class ="head-list"><a href="doctorschedule.php" class = "head-a">DOCTOR SCHEDULES</a></li>
+					<li class ="head-list"><a href="loginandappointment.php" class = "head-a">BOOK APPOINTMENT</a></li>
+					<li class ="head-list"><a href="loginandappointment.php" class = "head-a">LOGIN</a></li>
+				</ul>
+		</nav>		
+	</header>
+</body>
+</html>	
--- style.php
+++ style.php
@@ -1,34 +1,8 @@
-*{
+* {
+	padding :0;
 	margin:0;
-	padding:0;
-	background-color:LightGray;
 }
 
-div #header-{
-background-color:red;
-}
-#h2-title { 
-	background-color:red;
-}
-
-#h3-title-subscript {
-	color:"white";
-	line-height:0;
-}
-
-ul{
-	
-}
-
-ul li{
-	text-align:inline;
-	list-style:none;
-	display:inline;
-}
-ul li a{
-	text-decoration:none;
-	color:rgb(73,87,119);
-}
-ul li a:hover{
-	color:red;
+.logo { 
+	color:darkgrey;
 }
